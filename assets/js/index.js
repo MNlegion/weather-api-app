b80f4e2fc83a828239df0f72c9ac090b8cf6e3c3
MNlegion <rkreuser30@gmail.com>
2023-10-25 23:22:48 -0400

    user input to select city

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,24 @@
+const apiKey = "e9fe7b78654e60c910997496410c10f6";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
 
-const apiKey = 'e9fe7b78654e60c910997496410c10f6';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?&units=imperial&q=tampa&appid=' + apiKey;
+const userSearch = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  var data = await response.json();
+
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°F";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = Math.round(data.wind.speed) + " mph";
+  document.querySelector(".description").innerHTML = data.weather[0].description;
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(userSearch.value);
+});
 
 
-async function checkWeather() {
-    const response = await fetch(apiUrl);
-    var data = await response.json();
-
-    console.log(data);
-};
-
-checkWeather();
